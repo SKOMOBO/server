fdc7119c08b87ede236efb454cb945215132a783
Ulfasaar <weyers.ryan@gmail.com>
2017-07-17 21:56:10 +1200

    Updafted server routes

--- src/server.ts
+++ src/server.ts
@@ -12,41 +12,21 @@ import * as express from "express"
 
 export var server = express()
 
-// for url encoding
-// routes in form '/sensor?d=geopsjgp
-server.get('/', (req, resp)=>{
-    resp.send('Nothing to see here')
-})
-server.get('temp', (req, resp) =>{
-    
-})
-server.get('humidity', (req, resp) =>{
-    
-})
-server.get('pir', (req, resp) =>{
-    
-})
-
-//GET /dust?pm1=0&pm25=0&pm10=0
-server.get('dust', (req, resp) =>{
-    
-})
-server.get('co2', (req, resp) =>{
-    
-})
-
 // must be sent first
 server.get('id', (req, resp) =>{
     let id = req.param('d')
 })
 
 // then this one
+//  need to join with this somehow??
+// this should be easy to fill in during analysis if blank
 
-// /date?d=10,m=12,y=2017
+// need to put in seperate columns
 server.get('date', (req, resp)=>{
     let day = req.param('d')
     let month = req.param('m')
     let year = req.param('y')
+    resp.status(200)
 })
 
 // time
@@ -54,6 +34,43 @@ server.get('time', (req, resp) =>{
     let second = req.param('s')
     let minute = req.param('m')
     let hour = req.param('h')
+    resp.status(200)
+})
+
+
+// after this it doesnt matter
+
+
+// for url encoding
+// routes in form '/sensor?d=geopsjgp
+server.get('/', (req, resp)=>{
+    resp.send('Nothing to see here right now, maybe we will have a nice login and charts in the future')
+    resp.sendStatus(200)
+})
+server.get('temp', (req, resp) =>{
+    let temp = req.param('d')
+    resp.sendStatus(200)
+})
+server.get('humidity', (req, resp) =>{
+    let humidity = req.param('d')
+    resp.sendStatus(200)
+})
+server.get('pir', (req, resp) =>{
+    let pir = req.param('d')
+    resp.sendStatus(200)
+})
+
+//GET /dust?pm1=0&pm25=0&pm10=0
+server.get('dust', (req, resp) =>{
+    let pm1 = req.param('p1')
+    let pm25 = req.param('p25')
+    let pm10 = req.param('p10')
+    resp.sendStatus(200)
+})
+
+server.get('co2', (req, resp) =>{
+    let co2 = req.param('d')
+    resp.sendStatus(200)
 })
 // each sensor gets a different route
 
