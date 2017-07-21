f31dac22d3dead9ca3bc5f2f65c3003aad5f26bf
Ulfasaar <weyers.ryan@gmail.com>
2017-07-21 13:23:59 +1200

    Server now sends confirmation

--- src/server.ts
+++ src/server.ts
@@ -9,20 +9,23 @@ import {extract, config_db} from "./lib"
 }
 
 var server = net.createServer((socket)=>{
+    console.log("Server listening on: http://%s:%s", require("ip").address(), 80);
     let connection = config_db()
 
-    // socket.write("Connected\n")
+    socket.write("Connected")
     console.log("Connected");
+
     // let data = data.toString("UTF8")
     socket.on("data", (data)=>{
         console.log(data.toString("UTF8"))
         let values = extract(data.toString("UTF8"))
         console.log(values)
         store(values, connection)
-        
     })
 
     socket.pipe(socket)
+
+    // socket.end()
 })
 
 server.listen(80, '0.0.0.0');
