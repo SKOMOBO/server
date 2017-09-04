9259a700d728c180bc0548eea6497c82a7bfd723
Ulfasaar <weyers.ryan@gmail.com>
2017-09-04 15:22:17 +1200

    Added aws tcp port

--- src/tcp.ts
+++ src/tcp.ts
@@ -11,7 +11,8 @@ export var app = net.createServer((socket)=>{
     })
 })
 
-const port = 80
+// aws exposes port 80 using ip tables to forward to 8000
+const port = 8000
 app.listen(port, ()=>{
     console.log("Server listening on: %s:%s", require("ip").address(), port);
 })
