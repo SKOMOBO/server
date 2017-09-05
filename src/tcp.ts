8d14d07cfbcb1afab91723589bdf3287befbdf16
Ulfasaar <weyers.ryan@gmail.com>
2017-09-05 12:24:50 +1200

    Backing up the TCP to HTTP translation service

--- src/tcp.ts
+++ src/tcp.ts
@@ -11,6 +11,38 @@ export var app = net.createServer((socket)=>{
     })
 })
 
+import * as http from "http"
+
+
+/**
+ * This function passes the data to massey for storage
+ * 
+ * @param {string} data 
+ */
+function to_massey(data: string){
+
+    let options = {
+        host: "seat-skomobo.massey.ac.nz",
+        port: 80,
+        path: 'dynamic',
+        method: 'GET'
+    }
+
+    let route = ''
+    if(data[0] == '0'){
+        route = '/'
+    }else if(data[0] == '1'){
+        route = '/raspi_'
+    }
+
+    // remove first routing values
+    options.path = route + data.slice(2)
+    
+    http.get(options, (response)=>{
+        console.log(options.path)
+    })
+}
+
 // aws exposes port 80 using ip tables to forward to 8000
 const port = 8000
 app.listen(port, ()=>{
