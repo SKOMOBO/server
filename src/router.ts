45c628731c5ac7ae3a2d751e8c890f7f29029bac
Ulfasaar <weyers.ryan@gmail.com>
2017-11-04 11:08:09 +1300

    Added helmet to fix http headers for better security

--- src/router.ts
+++ src/router.ts
@@ -9,12 +9,15 @@ import {get_type} from "./database_manager"
 import {register} from "bugsnag"
 register(require("../../src/global_keys.json").bugsnag_key)
 
+var helmet = require("helmet")
 var compress = require("compression")
 export var app = express()
 
-// Enable GZIP compression
+// Enable GZIP compression for improved performance
 app.use(compress())
 
+// Fix headers to make things more secure
+app.use(helmet())
 
 //! look through here for abstraction oppurtunities
 //! create unit tests for all the things!!!
