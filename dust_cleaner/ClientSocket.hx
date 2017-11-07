7a9c176d8102d6b1d6d8cefbcb3445c924517344
Ulfasaar <weyers.ryan@gmail.com>
2017-11-07 22:46:56 +1300

    Getting closer to the cause I think might be something to do with constructor not being called

--- dust_cleaner/ClientSocket.hx
+++ dust_cleaner/ClientSocket.hx
@@ -7,35 +7,36 @@ import haxe.Json;
 @:keep @:expose
 class ClientSocket {
     
-    // private var prev_PM10:Float = 0.0;
-    // private  var prev_PM2_5:Float= 0.0;
+    private var prev_PM10:Float = 0.0;
+    private  var prev_PM2_5:Float= 0.0;
 
-    private var socket = new Socket();
+    private var socket:Socket;
     public var result: Any;
-    public function new(){}
+    public function new(){
+        socket = new Socket();
+        trace(socket);
+    }
 
-
-    // public function prep_data(pm10:Float, pm2_5:Float){
-    //     pm10 = pm10 / 1000;
-    //     pm2_5 = pm2_5 / 1000;
+    public function prep_data(pm10:Float, pm2_5:Float){
+        pm10 = pm10 / 1000;
+        pm2_5 = pm2_5 / 1000;
                 
-    //     var result = {"PM10": pm10, "PM2_5": pm2_5, "PM10_diff": pm10 - prev_PM10, "PM2_5_diff": pm2_5 - prev_PM2_5};
-    //     prev_PM10 = pm10;
-    //     prev_PM2_5 = pm2_5;
-    //     return result;
-    // }
+        var result = {"PM10": pm10, "PM2_5": pm2_5, "PM10_diff": pm10 - prev_PM10, "PM2_5_diff": pm2_5 - prev_PM2_5};
+        prev_PM10 = pm10;
+        prev_PM2_5 = pm2_5;
+        return result;
+    }
 
     // for some reason connect is undefined'
 
     // fix the nameing change to dust cleaner cliet
     public function clean(pm10:Float, pm2_5:Float){ 
-        trace(Socket);
-        socket.connect(9999, function(){
+        this.socket.connect(9999, function(){
             var data = prep_data(pm10, pm2_5);
-            socket.write(Json.stringify(data));
+            this.socket.write(Json.stringify(data));
         });
 
-        socket.on('data', function(data){
+        this.socket.on('data', function(data){
             result = Json.parse(data.toString('utf8'));
         });
     }
