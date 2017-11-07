e873379ae72a118c60ce1b5e08baaf76070e4826
Ulfasaar <weyers.ryan@gmail.com>
2017-11-07 22:37:10 +1300

    Troubleshooting not sure why its broken, may need to do bisect

--- dust_cleaner/ClientSocket.hx
+++ dust_cleaner/ClientSocket.hx
@@ -5,24 +5,29 @@ import haxe.Json;
 // rewrite this so that it becomes function call instead
 
 @:keep @:expose
-class DustCleanerClient {
+class ClientSocket {
+    
+    // private var prev_PM10:Float = 0.0;
+    // private  var prev_PM2_5:Float= 0.0;
+
     private var socket = new Socket();
     public var result: Any;
     public function new(){}
 
-    private var prev_PM10:Float = 0.0;
-    private  var prev_PM2_5:Float= 0.0;
 
-    public function prep_data(pm10:Float, pm2_5:Float):Any{
-        pm10 = pm10 / 1000;
-        pm2_5 = pm2_5 / 1000;
+    // public function prep_data(pm10:Float, pm2_5:Float){
+    //     pm10 = pm10 / 1000;
+    //     pm2_5 = pm2_5 / 1000;
                 
-        var result = {"PM10": pm10, "PM2_5": pm2_5, "PM10_diff": pm10 - prev_PM10, "PM2_5_diff": pm2_5 - prev_PM2_5};
-        prev_PM10 = pm10;
-        prev_PM2_5 = pm2_5;
-        return result;
-    }
+    //     var result = {"PM10": pm10, "PM2_5": pm2_5, "PM10_diff": pm10 - prev_PM10, "PM2_5_diff": pm2_5 - prev_PM2_5};
+    //     prev_PM10 = pm10;
+    //     prev_PM2_5 = pm2_5;
+    //     return result;
+    // }
 
+    // for some reason connect is undefined'
+
+    // fix the nameing change to dust cleaner cliet
     public function clean(pm10:Float, pm2_5:Float){ 
         trace(Socket);
         socket.connect(9999, function(){
