452af0799cecde62928a6384f7df41c140794107
Ulfasaar <weyers.ryan@gmail.com>
2017-11-07 20:27:26 +1300

    Integrated haxe better with vscode and setup unit testing

--- dust_cleaner/DustCleanerClient.hx
+++ dust_cleaner/DustCleanerClient.hx
@@ -4,12 +4,27 @@ import haxe.Json;
 
 // rewrite this so that it becomes function call instead
 
-
 @:keep @:expose
-class ClientSocket {
+class DustCleanerClient {
     private var socket = new Socket();
     public var result: Any;
     public function new(){}
+
+    private var prev_PM10:Float = 0.0;
+    private  var prev_PM2_5:Float= 0.0;
+
+    public function prep_data(pm10:Float, pm2_5:Float){
+        pm10 = pm10 / 1000;
+        pm2_5 = pm2_5 / 1000;
+                
+        var result = {"PM10": pm10, "PM2_5": pm2_5, "PM10_diff": pm10 - prev_PM10, "PM2_5_diff": pm2_5 - prev_PM2_5};
+        prev_PM10 = pm10;
+        prev_PM2_5 = pm2_5;
+        return result;
+         
+        // var data = {"PM10": pm10 / 1000.0, 
+        // "PM2_5": pm2_5/ 1000.0}
+    }
 
     public function clean(data){
         // var socket =  
--- dust_cleaner/MyTestRunner.hx
+++ dust_cleaner/MyTestRunner.hx
@@ -1,6 +1,17 @@
 class MyTest extends haxe.unit.TestCase{
-    public function testBasic() {
-        assertEquals("A", "A");
+    public function testPrepData() {
+        // assertEquals("A", "A");
+        var client = new DustCleanerClient();
+        var actual = client.prep_data(20, 20);
+        var expected = {"PM10": 0.02, "PM2_5": 0.02, "PM10_diff": 0.02, "PM2_5_diff": 0.02};
+        assertEquals(Std.string(expected), Std.string(actual));
+
+
+        //! make proper test for testing diff ie value should be different to 
+        // pm10 etc
+        expected = {"PM10": 0.01, "PM2_5": 0.01, "PM10_diff": -0.01, "PM2_5_diff": -0.01};
+        actual = client.prep_data(10, 10);
+        assertEquals(Std.string(expected), Std.string(actual));
     }
 }
 
