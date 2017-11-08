4592910d8a59b7a510cca391eac9595907c7cd60
Ulfasaar <weyers.ryan@gmail.com>
2017-11-08 15:18:21 +1300

    Scaffolding haxe port

---
+++ src/database_manager.hx
@@ -0,0 +1,9 @@
+package;
+
+//  use the std mysql driver for this compiled to javascript :)
+// nevermind its only for sys targets ugh, they should really make nodejs a sys target
+class database_manager {
+    public function new() {
+        
+    }
+}
---
+++ src/router.hx
@@ -0,0 +1,12 @@
+package;
+
+// just wrap existing nodejs code, make wrapper function inside lib
+// this makes it easy to make conditional for porting to other systems later
+// if necessary
+
+// would need to make wrapper objects for resp and req as well probably
+class router {
+    public function new() {
+        
+    }
+}
