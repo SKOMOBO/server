a7de7daf7904de383980161a14eae023d7dfa407
Ryan Weyers <weyers.ryan@gmail.com>
2018-04-19 17:52:35 +1200

    Changed port to 80 and removed iis

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const lib = require("./src/lib")
 
 const {app} = require("./src/router")
 
-let port = 81
+let port = 80
 
 app.listen(port, '0.0.0.0', function () {
     console.log("server listening")
